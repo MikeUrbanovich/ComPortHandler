98b4b7d96d0d8496a3722e1d26c759abde19cd9d
MichaelUrbanovich <mi.urbanovich@gmail.com>
2022-07-11 12:47:32 +0300

    init

---
+++ ComPortHandler/Program.cs
@@ -0,0 +1,24 @@
+﻿using ComPortHandler.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<InfluxDBService>();
+    })
+    .Build();
+
+var db = host.Services.GetRequiredService<InfluxDBService>();
+
+db.Write("");
+
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+int keyOneValue = config.GetValue<int>("KeyOne");
+
+Console.WriteLine(keyOneValue);
+Console.ReadKey();
+
+
---
+++ ComPortHandler/Services/InfluxDBService.cs
@@ -0,0 +1,34 @@
+﻿using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+using Microsoft.Extensions.Configuration;
+
+namespace ComPortHandler.Services
+{
+    public class InfluxDBService
+    {
+        private readonly string _token;
+        private readonly string _host;
+
+        public InfluxDBService(IConfiguration configuration)
+        {
+            _token = configuration.GetValue<string>("InfluxDB:Token");
+            _host = configuration.GetValue<string>("InfluxDB:Host");
+        }
+
+        public void Write(string data)
+        {
+            using var client = InfluxDBClientFactory.Create(_host, _token);
+            using var write = client.GetWriteApi();
+
+            var point = PointData.Measurement("com_port_data")
+                    .Tag("PortNumber", "testNumber")
+                    .Field("PortData", 55D)
+                    .Timestamp(DateTime.UtcNow, WritePrecision.Ns);
+
+            write.WritePoint(point, "new_test_bucket", "murbCompany");
+
+            var tmp = "";
+        }
+    }
+}
